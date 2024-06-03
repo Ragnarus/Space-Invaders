28d32db96e9c8d30ae5cf5b8e4e5d3c4966edd9b
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2024-06-03 12:45:07 +0200

    Updated Startscreen
    Updated Startscreen: added buttons for navigation, added string ressources for the text of the buttons, added rounded corner style for buttons.

--- app/src/main/java/com/example/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/example/spaceinvaders/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("some_int", 0);
+
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragmentContainer, StartScreenFragment.class, bundle)
+                    .commit();
+        }
     }
 }
