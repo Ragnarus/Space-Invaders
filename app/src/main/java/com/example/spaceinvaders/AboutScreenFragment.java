3861f95bd18f065b970eaa850a96ded731eb1e80
Alexander Krampl <119439649+Ragnarus@users.noreply.github.com>
2024-08-04 19:10:52 +0200

    About us

--- app/src/main/java/com/example/spaceinvaders/AboutScreenFragment.java
+++ app/src/main/java/com/example/spaceinvaders/AboutScreenFragment.java
@@ -1,5 +1,7 @@
 package com.example.spaceinvaders;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +9,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AboutScreenFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AboutScreenFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AboutScreenFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutScreenFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutScreenFragment newInstance(String param1, String param2) {
-        AboutScreenFragment fragment = new AboutScreenFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_screen, container, false);
+        TextView link = view.findViewById(R.id.textViewLink);
+        link.setOnClickListener(v -> {
+            String url = "https://github.com/Ragnarus/Space-Invaders";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        });
+
+
+        return view;
     }
 }
